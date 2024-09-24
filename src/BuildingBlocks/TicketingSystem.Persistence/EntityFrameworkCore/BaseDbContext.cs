8c6d8b3ded9e54c983acdbe5e5331d1b3d8ad786
thanhphongdev014 <phongjin014@gmail.com>
2024-09-24 11:59:40 +0700

    feat: add audit object, soft delete filter

--- src/BuildingBlocks/TicketingSystem.Persistence/EntityFrameworkCore/BaseDbContext.cs
+++ src/BuildingBlocks/TicketingSystem.Persistence/EntityFrameworkCore/BaseDbContext.cs
@@ -1,10 +1,32 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TicketingSystem.Domain.Entities;
 
 namespace TicketingSystem.Persistence.EntityFrameworkCore;
 public class BaseDbContext : DbContext
 {
-    public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        base.OnModelCreating(modelBuilder);
+        var softDeleteEntities = typeof(ISoftDelete).Assembly.GetTypes()
+                .Where(type => typeof(ISoftDelete).IsAssignableFrom(type)
+                    && type is { IsClass: true, IsAbstract: false });
+
+        foreach (var softDeleteEntity in softDeleteEntities)
+        {
+            modelBuilder.Entity(softDeleteEntity).HasQueryFilter(
+                  GenerateQueryFilterLambda(softDeleteEntity));
+        }
+    }
+
+    private LambdaExpression GenerateQueryFilterLambda(Type type)
+    {
+        var parameter = Expression.Parameter(type, "w");
+        var falseConstantValue = Expression.Constant(false);
+        var propertyAccess = Expression.PropertyOrField(parameter, nameof(ISoftDelete.IsDeleted));
+        var equalExpression = Expression.Equal(propertyAccess, falseConstantValue);
+        var lambda = Expression.Lambda(equalExpression, parameter);
+
+        return lambda;
     }
 }
---
+++ src/BuildingBlocks/TicketingSystem.Persistence/Interceptors/AuditObjectInterceptor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using TicketingSystem.Domain.Entities;
+
+namespace TicketingSystem.Persistence.Interceptors;
+public class AuditObjectInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(
+        DbContextEventData eventData,
+        InterceptionResult<int> result)
+    {
+        if (eventData.Context is null) return result;
+
+        foreach (var entry in eventData.Context.ChangeTracker.Entries())
+        {
+            if (entry is { State: EntityState.Added, Entity: IAuditedObject addedObject })
+            {
+                addedObject.CreationTime = DateTimeOffset.Now;
+            }
+            if (entry is { State: EntityState.Modified, Entity: IAuditedObject modifiedObject })
+            {
+                modifiedObject.LastModificationTime = DateTimeOffset.Now;
+            }
+        }
+        return result;
+    }
+}
