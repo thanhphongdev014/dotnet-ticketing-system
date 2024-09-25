18bc36fef4bfe93c514feecdd1911a3052c1e2ed
thanhphongdev014 <phongjin014@gmail.com>
2024-09-26 00:59:08 +0700

    feat: implement repository

---
+++ src/BuildingBlocks/TicketingSystem.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,60 @@
+﻿namespace TicketingSystem.Domain.Exceptions;
+
+/// <summary>
+/// This exception is thrown if an entity is expected to be found but not found.
+/// </summary>
+public class EntityNotFoundException : Exception
+{
+    /// <summary>
+    /// Type of the entity.
+    /// </summary>
+    public Type? EntityType { get; set; }
+
+    /// <summary>
+    /// ID of the Entity.
+    /// </summary>
+    public object? Id { get; set; }
+
+    /// <summary>
+    /// Creates a new <see cref="EntityNotFoundException"/> object.
+    /// </summary>
+    public EntityNotFoundException()
+    {
+
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="EntityNotFoundException"/> object.
+    /// </summary>
+    public EntityNotFoundException(Type entityType, object? id = null, Exception? innerException = null)
+        : base(
+            id == null
+                ? $"There is no such an entity given id. Entity type: {entityType.FullName}"
+                : $"There is no such an entity. Entity type: {entityType.FullName}, id: {id}",
+            innerException)
+    {
+        EntityType = entityType;
+        Id = id;
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="EntityNotFoundException"/> object.
+    /// </summary>
+    /// <param name="message">Exception message</param>
+    public EntityNotFoundException(string message)
+        : base(message)
+    {
+
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="EntityNotFoundException"/> object.
+    /// </summary>
+    /// <param name="message">Exception message</param>
+    /// <param name="innerException">Inner exception</param>
+    public EntityNotFoundException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+
+    }
+}
---
+++ src/BuildingBlocks/TicketingSystem.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TicketingSystem.Domain.Entities;
+using TicketingSystem.Domain.Exceptions;
+using TicketingSystem.Persistence.EntityFrameworkCore;
+
+namespace TicketingSystem.Persistence.Repositories;
+public class BaseRepository<TEntity, TKey>(BaseDbContext dbContext) where TEntity : Entity<TKey>, IAggregateRoot
+{
+    private readonly BaseDbContext _dbContext = dbContext;
+
+    public async Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>>? predicate = null, CancellationToken cancellationToken = default)
+    {
+        return predicate != null ?
+            await _dbContext.Set<TEntity>().AsNoTracking().FirstOrDefaultAsync(predicate, cancellationToken) :
+            await _dbContext.Set<TEntity>().AsNoTracking().FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public async Task<TEntity> GetAsync(TKey id)
+    {
+        var entity = await _dbContext.Set<TEntity>().AsNoTracking().FirstOrDefaultAsync(x => x.Id!.Equals(id));
+        if (entity == null)
+        {
+            throw new EntityNotFoundException(typeof(TEntity), id);
+        }
+        return entity;
+    }
+
+    public Task<List<TEntity>> GetListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<TEntity> GetQueryableAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
