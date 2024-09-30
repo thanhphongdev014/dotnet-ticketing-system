cd6a665b012a2efa37b1b5a4b67d52e1d11f1c7a
thanhphongdev014 <phongjin014@gmail.com>
2024-09-30 23:50:44 +0700

    feat: add event service domain

---
+++ src/Services/Events/TicketingSystem.Services.Event.Domain/Entities/Event.cs
@@ -0,0 +1,13 @@
+﻿using TicketingSystem.Domain.Entities;
+
+namespace TicketingSystem.Services.Event.Domain.Entities;
+
+public class Event(Guid id, string name, DateTimeOffset startDate, DateTimeOffset endDate)
+    : Entity<Guid>(id), IAggregateRoot, IAuditedObject
+{
+    public string Name { get; set; } = name;
+    public DateTimeOffset StartDate { get; set; } = startDate;
+    public DateTimeOffset EndDate { get; set; } = endDate;
+    public DateTimeOffset LastModificationTime { get; set; }
+    public DateTimeOffset CreationTime { get; set; }
+}
