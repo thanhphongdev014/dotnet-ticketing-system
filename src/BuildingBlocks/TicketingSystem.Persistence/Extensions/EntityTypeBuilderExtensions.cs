44f6df4902c26071ab305b728edf396517d85d05
thanhphongdev014 <phongjin014@gmail.com>
2024-10-02 21:24:19 +0700

    feat: add entity config

---
+++ src/BuildingBlocks/TicketingSystem.Persistence/Extensions/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using TicketingSystem.Domain.Entities;
+
+namespace TicketingSystem.Persistence.Extensions;
+
+public static class EntityTypeBuilderExtensions
+{
+    public static void ConfigureByConvention(this EntityTypeBuilder b)
+    {
+        b.TryConfigureSoftDelete();
+        b.TryConfigureConcurrencyStamp();
+    }
+
+    private static void TryConfigureConcurrencyStamp(this EntityTypeBuilder b)
+    {
+        if (b.Metadata.ClrType.IsAssignableTo(typeof(IHasConcurrencyStamp)))
+        {
+            b.Property(nameof(IHasConcurrencyStamp.ConcurrencyStamp))
+                .IsConcurrencyToken()
+                .HasMaxLength(40)
+                .HasColumnName(nameof(IHasConcurrencyStamp.ConcurrencyStamp));
+        }
+    }
+
+    private static void TryConfigureSoftDelete(this EntityTypeBuilder b)
+    {
+        if (b.Metadata.ClrType.IsAssignableTo(typeof(ISoftDelete)))
+        {
+            b.Property(nameof(ISoftDelete.IsDeleted))
+                .IsRequired()
+                .HasDefaultValue(false)
+                .HasColumnName(nameof(ISoftDelete.IsDeleted));
+        }
+    }
+}
