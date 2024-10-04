edd29e2709eaad0fb5d5c5079c2ab3901054bf2d
thanhphongdev014 <phongjin014@gmail.com>
2024-10-04 23:45:35 +0700

    feat: add business rule

---
+++ src/BuildingBlocks/TicketingSystem.Domain/BusinessRules/BusinessRuleValidationException.cs
@@ -0,0 +1,13 @@
+﻿namespace TicketingSystem.Domain.BusinessRules;
+
+public class BusinessRuleValidationException(IBusinessRule brokenRule) : Exception(brokenRule.Message)
+{
+    public IBusinessRule BrokenRule { get; } = brokenRule;
+
+    public string Details { get; } = brokenRule.Message;
+
+    public override string ToString()
+    {
+        return $"{BrokenRule.GetType().FullName}: {BrokenRule.Message}";
+    }
+}
---
+++ src/BuildingBlocks/TicketingSystem.Domain/BusinessRules/IBussinessRule.cs
@@ -0,0 +1,8 @@
+﻿namespace TicketingSystem.Domain.BusinessRules;
+
+public interface IBusinessRule
+{
+    Task<bool> IsBroken();
+
+    string Message { get; }
+}
