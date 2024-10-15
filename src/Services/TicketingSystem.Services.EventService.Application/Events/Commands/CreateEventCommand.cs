1139d9cd69f2c10b0550f960734386c60d56de68
thanhphongdev014 <phongjin014@gmail.com>
2024-10-15 23:57:24 +0700

    feat: add create event command

---
+++ src/Services/TicketingSystem.Services.EventService.Application/Events/Commands/CreateEventCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace TicketingSystem.Services.EventService.Application.Events.Commands;
+
+public class CreateEventCommand : IRequest
+{
+    
+}
+public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand>
+{
+    public Task Handle(CreateEventCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
