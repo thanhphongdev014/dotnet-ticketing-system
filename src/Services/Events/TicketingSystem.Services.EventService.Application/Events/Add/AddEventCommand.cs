c3e787b6d2efc94b281b785d2e9d8748b19dc134
thanhphongdev014 <phongjin014@gmail.com>
2024-10-17 23:55:57 +0700

    feat: add adding event validator

---
+++ src/Services/Events/TicketingSystem.Services.EventService.Application/Events/Add/AddEventCommand.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+
+namespace TicketingSystem.Services.EventService.Application.Events.Add;
+
+public class AddEventCommand(
+    string name,
+    string street,
+    string district,
+    string city,
+    DateTimeOffset startDate,
+    DateTimeOffset endDate,
+    List<string> imageNames) : IRequest
+{
+    public string Name { get; set; } = name;
+    public string Street { get; set; } = street;
+    public string District { get; set; } = district;
+    public string City { get; set; } = city;
+    public DateTimeOffset StartDate { get; set; } = startDate;
+    public DateTimeOffset EndDate { get; set; } = endDate;
+    public List<string> ImageNames { get; set; } = imageNames;
+}
+
+public class AddEventCommandHandler : IRequestHandler<AddEventCommand>
+{
+    public Task Handle(AddEventCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Events/TicketingSystem.Services.EventService.Application/Events/Add/AddEventCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace TicketingSystem.Services.EventService.Application.Events.Add;
+
+public class AddEventCommandValidator : AbstractValidator<AddEventCommand>
+{
+    public AddEventCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Event name is empty");
+    }
+}
