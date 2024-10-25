014d877b0927d19080286e8b564f061a11bae46b
thanhphongdev014 <phongjin014@gmail.com>
2024-10-25 23:08:07 +0700

    feat: delete event handler

---
+++ src/Services/Events/TicketingSystem.Services.EventService.Application/Events/Delete/DeleteEventCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using TicketingSystem.Domain.Repositories;
+using TicketingSystem.Services.EventService.Domain.Events;
+
+namespace TicketingSystem.Services.EventService.Application.Events.Delete;
+
+public class DeleteEventCommand(
+    Guid eventId) : IRequest
+{
+    public Guid EventId { get; set; } = eventId;
+}
+
+public class UpdateEventCommandHandler(
+    IRepository<Event, Guid> eventRepository) : IRequestHandler<DeleteEventCommand>
+{
+    public async Task Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+    {
+        var eventEntity = await eventRepository.GetAsync(request.EventId);
+        await eventRepository.DeleteAsync(eventEntity, cancellationToken);
+        //delete image later
+    }
+}
