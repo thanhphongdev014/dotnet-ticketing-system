62f0ec1a41360ec1f5e8e1a9de70dbca413b98bc
thanhphongdev014 <phongjin014@gmail.com>
2024-10-29 23:22:18 +0700

    feat: add endpoint handler

---
+++ src/BuildingBlocks/TicketingSystem.Infrastructure/WebApi/MinimalApi/IEndpointHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Routing;
+
+namespace TicketingSystem.Infrastructure.WebApi.MinimalApi;
+
+public interface IEndpointHandler
+{
+    static abstract void MapEndpoint(IEndpointRouteBuilder builder);
+}
+
+public static class EndpointRouteBuilderExtension
+{
+    public static void MapEndpointHandlers(this IEndpointRouteBuilder builder, Assembly assembly)
+    {
+        var endpointHandlerTypes = assembly
+            .GetTypes()
+            .Where(x => x.GetInterfaces().Contains(typeof(IEndpointHandler)))
+            .ToList();
+
+        foreach (var item in endpointHandlerTypes)
+        {
+            item.InvokeMember(nameof(IEndpointHandler.MapEndpoint), BindingFlags.InvokeMethod, null, null,
+                new object?[] { builder });
+        }
+    }
+}
